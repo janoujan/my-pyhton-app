a5618e5c9f78a3a659cc0ebc73e8feaf71a9499b
janoujan <janou@janou.pro>
2025-03-17 12:11:53 +0100

    add bad .main

--- test_main.py
+++ test_main.py
@@ -3,3 +3,8 @@ from main import add
 def test_add():
  assert add(2, 3) == 5
  assert add(-1, 1) == 0
+ 
+ def test_multiply():
+  assert multiply(2, 3) == 6
+  assert multiply(-1,1) == -1
+  assert multiply(0, -1) == 0
